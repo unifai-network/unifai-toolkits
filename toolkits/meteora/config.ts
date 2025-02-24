0cbe937429bfaf67678509c96279ab56dbbfa516
Searoc <searoc.song@gmail.com>
2025-02-24 17:33:30 +0800

    Remove test env

--- toolkits/meteora/config.ts
+++ toolkits/meteora/config.ts
@@ -7,6 +7,5 @@ dotenv.config();
 export const toolkit = new Toolkit({ apiKey: process.env.TOOLKIT_API_KEY });
 
 export const txApi = new TransactionAPI({ apiKey: process.env.TOOLKIT_API_KEY });
-txApi.setEndpoint(process.env.UNIFAI_TRANSACTION_API_ENDPOINT);
 
 export const connection = new Connection(process.env.SOLANA_RPC_URL || clusterApiUrl('mainnet-beta'), 'confirmed');
