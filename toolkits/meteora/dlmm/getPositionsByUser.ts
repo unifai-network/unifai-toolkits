bf7c606ec7c0a6f0fb5090dc3381f9c73be5ba15
Searoc <searoc.song@gmail.com>
2025-02-24 16:17:40 +0800

    Fix ui amount for meteora

--- toolkits/meteora/dlmm/getPositionsByUser.ts
+++ toolkits/meteora/dlmm/getPositionsByUser.ts
@@ -1,7 +1,9 @@
 import { ActionContext } from "unifai-sdk";
 import { connection, toolkit } from "../config";
-import DLMM from "@meteora-ag/dlmm";
+import DLMM, { PositionInfo } from "@meteora-ag/dlmm";
 import { PublicKey } from "@solana/web3.js";
+import { toUiAmount } from "../utils";
+import BN = require("bn.js");
 
 toolkit.action(
   {
@@ -19,7 +21,41 @@ toolkit.action(
     try {
       const result = await DLMM.getAllLbPairPositionsByUser(connection, new PublicKey(payload.userPublicKey), { cluster: 'mainnet-beta' }).then(res => {
         return {
-          lbPairPositions: Array.from(res.values()),
+          lbPairPositions: Array.from(res.values()).map(info => ({
+            lbPair: {
+              lbPairPublicKey: info.publicKey.toString(),
+              binStep: info.lbPair.binStep,
+              creator: info.lbPair.creator.toString(),
+              reserveX: info.lbPair.reserveX.toString(),
+              reserveY: info.lbPair.reserveY.toString(),
+              tokenXMint: info.lbPair.tokenXMint.toString(),
+              tokenYMint: info.lbPair.tokenYMint.toString(),
+              totalXAmount: toUiAmount(new BN(info.tokenX.amount.toString()), info.tokenX.decimal),
+              totalYAmount: toUiAmount(new BN(info.tokenY.amount.toString()), info.tokenY.decimal),
+              baseFeeBps: DLMM.calculateFeeInfo(info.lbPair.parameters.baseFactor, info.lbPair.binStep).baseFeeRatePercentage.mul(100).toNumber(),
+              protocolFeeBps: info.lbPair.parameters.protocolShare,
+            },
+            positions: info.lbPairPositionsData.map(position => ({
+              positionPublicKey: position.publicKey.toString(),
+              feeOwner: position.positionData.feeOwner.toString(),
+              feeX: toUiAmount(position.positionData.feeX, info.tokenX.decimal),
+              feeY: toUiAmount(position.positionData.feeY, info.tokenY.decimal),
+              totalXAmount: toUiAmount(new BN(position.positionData.totalXAmount.split('.')[0]), info.tokenX.decimal),
+              totalYAmount: toUiAmount(new BN(position.positionData.totalYAmount.split('.')[0]), info.tokenY.decimal),
+              lowerBinId: position.positionData.lowerBinId,
+              upperBinId: position.positionData.upperBinId,
+              totalClaimedFeeXAmount: toUiAmount(position.positionData.totalClaimedFeeXAmount, info.tokenX.decimal),
+              totalClaimedFeeYAmount: toUiAmount(position.positionData.totalClaimedFeeYAmount, info.tokenY.decimal),
+              positionBinData: position.positionData.positionBinData.map(bin => ({
+                binId: bin.binId,
+                pricePerToken: bin.pricePerToken,
+                binXAmount: toUiAmount(new BN(bin.binXAmount.split('.')[0]), info.tokenX.decimal),
+                binYAmount: toUiAmount(new BN(bin.binYAmount.split('.')[0]), info.tokenY.decimal),
+                positionXAmount: toUiAmount(new BN(bin.positionXAmount.split('.')[0]), info.tokenX.decimal),
+                positionYAmount: toUiAmount(new BN(bin.positionYAmount.split('.')[0]), info.tokenY.decimal),
+              })),
+            })),
+          })),
         }
       });
       return ctx.result(result);
@@ -27,4 +63,4 @@ toolkit.action(
       return ctx.result({ error: `Failed to get position infomation: ${error}` });
     }
   }
-)
+);
--- toolkits/meteora/dynamic/getPool.ts
+++ toolkits/meteora/dynamic/getPool.ts
@@ -2,6 +2,7 @@ import { ActionContext } from "unifai-sdk";
 import { connection, toolkit } from "../config";
 import { PublicKey } from "@solana/web3.js";
 import AmmImpl from "@mercurial-finance/dynamic-amm-sdk";
+import { toUiAmount } from "../utils";
 
 toolkit.action(
   {
@@ -23,17 +24,14 @@ toolkit.action(
         poolAddress: amm.address.toString(),
         baseMint: amm.tokenAMint.address.toString(),
         quoteMint: amm.tokenBMint.address.toString(),
-        baseDecimals: amm.tokenAMint.decimals.toString(),
-        quoteDecimals: amm.tokenBMint.decimals.toString(),
         baseReserve: amm.vaultA.vaultPda.toString(),
         quoteReserve: amm.vaultB.vaultPda.toString(),
-        baseAmount: amm.poolInfo.tokenAAmount.toString(),
-        quoteAmount: amm.poolInfo.tokenBAmount.toString(),
+        baseAmount: toUiAmount(amm.poolInfo.tokenAAmount, amm.tokenAMint.decimals),
+        quoteAmount: toUiAmount(amm.poolInfo.tokenBAmount, amm.tokenBMint.decimals),
         virtualPrice: amm.poolInfo.virtualPrice,
         lpMint: amm.poolState.lpMint.toString(),
-        lpDecimals: amm.decimals,
-        lpSupply: amm.poolState.lpSupply.toString(),
-        totalLockedLp: amm.poolState.totalLockedLp.toString(),
+        lpSupply: toUiAmount(amm.poolState.lpSupply, amm.decimals),
+        totalLockedLp: toUiAmount(amm.poolState.totalLockedLp, amm.decimals),
         feeBps: amm.feeBps.toString(),
       });
     } catch (error) {
